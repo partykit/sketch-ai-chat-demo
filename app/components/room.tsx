bb7daedc601533d1027847a7ce677ed8d1bb0262
Matt Webb <matt@interconnected.org>
2023-10-26 10:26:14 +0100

    Removes chatroom backend

--- app/components/room.tsx
+++ app/components/room.tsx
@@ -3,7 +3,6 @@ import { useUser } from "~/providers/user-context";
 import type { Message } from "~/shared";
 import AddMessageForm from "./add-message-form";
 import Messages from "./messages";
-import usePartySocket from "partysocket/react";
 
 export default function Room(props: { host: string; roomName: string }) {
   const [messages, setMessages] = useState<Message[]>([]);
@@ -24,23 +23,8 @@ export default function Room(props: { host: string; roomName: string }) {
     return [...prevMessages, message];
   };
 
-  const socket = usePartySocket({
-    host: props.host,
-    party: "chatRoom",
-    room: props.roomName,
-    onMessage(evt) {
-      const data = JSON.parse(evt.data);
-      if (data.type === "history") {
-        setMessages(data.messages);
-      } else if (data.type === "update") {
-        setMessages((prevMessages) => handleUpdate(prevMessages, data.message));
-      }
-    },
-  });
-
   const addMessage = (message: Message) => {
     setMessages((prevMessages) => [...prevMessages, message]);
-    socket.send(JSON.stringify({ type: "message", message }));
   };
 
   return (
