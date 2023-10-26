703dcaa8d628c0ece1283312f1ba0a3e764384d1
Matt Webb <matt@interconnected.org>
2023-10-26 15:42:30 +0100

    Safari fix

--- app/components/header-overlay.tsx
+++ app/components/header-overlay.tsx
@@ -12,7 +12,10 @@ export default function Overlay(props: {
   };
 
   return (
-    <div className="z-10 absolute top-0 left-0 bottom-0 right-0 bg-black/70 backdrop-blur-sm flex justify-center items-center">
+    <div
+      style={{ WebkitBackdropFilter: "blur(5px)" }}
+      className="z-10 absolute top-0 left-0 bottom-0 right-0 bg-black/70 backdrop-blur-sm flex justify-center items-center"
+    >
       <div className="text-white justify-center items-center flex flex-col gap-12">
         <SetUsernameForm setUsername={handleSetUserName} />
         <button className="text-sm" onClick={() => props.dismiss()}>
